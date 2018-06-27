d05259094a69c6c7aaa5b97ecf42add5d54e4e8d
PullingStrings <drammatik@gmail.com>
2018-06-27 12:14:58 +0100

    boilerplate

---
+++ src/About/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+    import './About.css'
+
+
+class About extends Component {
+    // The render method contains the JSX code which will be compiled to HTML.
+    render() {
+        return (
+            <div className="today--section container">
+
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+    import './NavBar.css'
+
+
+class NavBar extends Component {
+
+render() {
+    return (
+      <main>
+        <div class="mysite">
+          <div class="container">
+            <h1 class="display-4">Fluid jumbotron1</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
+        <div class="fuse">
+          <div class="container">
+            <h1 class="display-4">Fluid jumbotron2</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
+        <div class="blog">
+          <div class="container">
+            <h1 class="display-4">Fluid jumbotron3</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
+      </main>
+
+    )
+  }
+}
+
+
+    export default NavBar;
